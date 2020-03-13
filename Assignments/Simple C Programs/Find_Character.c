66e8a5a55848ffe90fb345c35f727533541745fa
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-13 23:26:55 +0530

    Program to find the character using it's ASCII value

---
+++ Assignments/Simple C Programs/Find_Character.c
@@ -0,0 +1,25 @@
+/*
+
+Q. Program to display equivalent character.
+
+*/
+
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	printf("Enter ASCII Value : ");
+	scanf("%d", &a);
+	printf("Equivalent character = %c\n", a);
+}
+
+/*  OUTPUT
+
+$ cc Find_Character.c
+$ ./a.out
+Enter ASCII Value : 65
+Equivalent character = A 
+ 
+*/
+
